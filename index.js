ac30b234281e623a9f0894dee8a0d6502208660f
John Tarleton Moore <tarletonmoore@gmail.com>
2022-03-05 16:58:21 -0500

    commit tree 48ed36bac16c5a75aa59a8ba775d6236554402c6

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let array = []
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return array
+}
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--)
+    }
+}
